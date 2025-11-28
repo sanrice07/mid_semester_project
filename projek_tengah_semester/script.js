122f0a64b8d9299832a122c76fa83da4e9d32ac2
sanrice07 <ikhsansalmandev@gmail.com>
2025-11-28 20:13:37 +0700

    Memperbarui file

--- projek_tengah_semester/script.js
+++ projek_tengah_semester/script.js
@@ -1,1 +1,23 @@
-console.log("Website Napoleon Bonaparte Loaded");
+
+    const slides = document.querySelector(".slides");
+    const totalSlides = document.querySelectorAll(".slides img").length;
+
+    let index = 0;
+
+    function changeSlide() {
+        index++;
+        if (index >= totalSlides) index = 0;
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(changeSlide, 5000);
+
+ 
+		const tombol = document.querySelector('.tombol');
+		const menu = document.querySelector('.menu');
+ 
+		tombol.addEventListener('click', () => {
+			menu.classList.toggle('aktif');
+		});
+
+
