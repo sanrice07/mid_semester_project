b7378fff320676aef5bb13c1e87a605abeaf4a61
sanrice07 <ikhsansalmandev@gmail.com>
2025-11-29 11:41:27 +0700

    Merapihkan file

--- projek_tengah_semester/script.js
+++ projek_tengah_semester/script.js
@@ -1,23 +1,23 @@
 
-    const slides = document.querySelector(".slides");
-    const totalSlides = document.querySelectorAll(".slides img").length;
+const slides = document.querySelector(".slides");
+const totalSlides = document.querySelectorAll(".slides img").length;
 
-    let index = 0;
+let index = 0;
 
-    function changeSlide() {
-        index++;
-        if (index >= totalSlides) index = 0;
-        slides.style.transform = `translateX(-${index * 100}%)`;
-    }
+function changeSlide() {
+    index++;
+    if (index >= totalSlides) index = 0;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
 
-    setInterval(changeSlide, 5000);
+setInterval(changeSlide, 5000);
 
- 
-		const tombol = document.querySelector('.tombol');
-		const menu = document.querySelector('.menu');
- 
-		tombol.addEventListener('click', () => {
-			menu.classList.toggle('aktif');
-		});
 
-        document.getElementById("year").textContent = new Date().getFullYear();
+const tombol = document.querySelector('.tombol');
+const menu = document.querySelector('.menu');
+
+tombol.addEventListener('click', () => {
+    menu.classList.toggle('aktif');
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
